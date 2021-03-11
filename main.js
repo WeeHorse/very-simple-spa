287f5fa101343f9e3cf1e56a60129a8d15b6344c
Benjamin Berglund <benjaminberglund@gmail.com>
2021-03-11 11:28:47 +0100

    Added page-3 code

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ function changePage(){
   }else if(location.hash === "#page-2"){
     $('main').html('<h1>This is page 2</h1>');
 
+  }else if(location.hash === "#page-3"){
+    $('main').html('<h1>This is page 3</h1>');
+    $('main').append('<p>Special feature 3</p>')
+
   }else{
     $('main').html('<h1>Welcome to default page</h1>');
   }
